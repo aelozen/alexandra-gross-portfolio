c10f0f12d370d97a568b543ac4c84a22c713a4d7
aelozen <alexgross95@gmail.com>
2023-07-31 00:35:45 -0400

    add research page

--- public/assets/js/peek.js
+++ public/assets/js/peek.js
@@ -1,22 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const characterImg = document.querySelector(".character-container img");
-    const containerHeight = document.querySelector(".character-container").clientHeight;
-    const peekAmount = 30;
+    document.addEventListener("DOMContentLoaded", function() {
+        const characterImg = document.querySelector(".character-container img");
+        const peekButton = document.querySelector(".peek-button");
+        const containerHeight = document.querySelector(".character-container").clientHeight;
+        const peekAmount = 35;
 
-    function peekIn() {
-        characterImg.style.top = `-${peekAmount}px`;
+        function peekIn() {
+            characterImg.style.top = `-${peekAmount}px`;
+            peekButton.style.display = 'block'; 
+            peekButton.disabled = false; 
+        }
+
+        function peekOut() {
+            characterImg.style.top = `-${containerHeight}px`;
+            peekButton.style.display = 'none'; 
+            peekButton.disabled = true; 
+        }
+
+        // Peeking animation loop
+        function animatePeek() {
+            peekIn();
+            setTimeout(peekOut, 1000); // Adjust how long waldo peeks
+            setTimeout(animatePeek, 4000); // Adjust how often waldo peeks
+        }
+
+        animatePeek(); 
+    });
+
+    function onButtonClick() {
+        window.open("/pages/research/research.html", "_blank");
     }
-
-    function peekOut() {
-        characterImg.style.top = `${-containerHeight}px`;
-    }
-
-    // Peeking animation loop
-    function animatePeek() {
-        peekIn();
-        setTimeout(peekOut, 1000); // Adjust how long waldo peeks
-        setTimeout(animatePeek, 5000); // Adjust how often waldo peeks
-    }
-
-    animatePeek();
-});
